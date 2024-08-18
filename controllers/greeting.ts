e50fe1025121145d8e99033c958211ef33301216
shobhit <shobhitmishra2002@gmail.com>
2024-08-18 17:22:29 +0530

    refactor: removed that temp change

--- controllers/greeting.ts
+++ controllers/greeting.ts
@@ -1,13 +1,9 @@
 import { Request, Response } from "express";
-import User from "../schema/user.schema";
 
 // write your controllers here...
 
 export const greeting = async (req: Request, res: Response) => {
-  const users = await User.find({});
-
   res.status(200).json({
     message: "Hi",
-    users,
   });
 };
